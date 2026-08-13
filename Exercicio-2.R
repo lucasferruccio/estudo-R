d7418ea0aba51dc171518ec49763e4f042155797
Lucas Ferruccio <lucaspaiva.ufrpe@gmail.com>
2026-08-12 22:05:34 -0300

    Respostas segunda lista

---
+++ Exercicio-2.R
@@ -0,0 +1,32 @@
+x <- 133
+y <- 36
+
+z = x + y
+
+z_sqrt = sqrt(z)
+
+z_log = log(z_sqrt)
+
+z_round = round(z_log)
+
+sum <- 0 + 1 + 1 + 2 + 3
+
+round(log(sum), digits = 3)
+
+f = round(cos(2) + 1, digits = 3)
+
+fatorial <- round(log10(factorial(10)), digits = 2)
+
+c = 300000000
+
+m = 10
+
+E = m * (c^2)
+
+R = round(log(E), 2)
+
+v <- c(2,5,7,8,9)
+
+vetor_novo <- v^3 + v^2 + v
+
+mean(vetor_novo)
